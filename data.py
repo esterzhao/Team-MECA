7ae9ec75361a3c20f8187dc088e5c72b49e27d7b
Claire Sun <csun@brandeis.edu>
2019-02-10 03:22:01 -0500

    Now calculates what hospital to go to

--- data.py
+++ data.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+
+import math
+import argparse
+import numpy
 
 #city: list[hospital,%Accurate Prenatal Care]
 city_to_hospital_dict = {
@@ -75,7 +81,7 @@ city_to_infant_mortality_rate = {
     "Worcester":8.8
 }
 
-#1991-2009 (18 years)
+#1991-2009
 race_to_infant_mortality_rate = {
     "White":[5.5,5.5,5.3,5.3,4.4,4.7,4.8,4.6,4.7,3.8,4.1,4.1,4.1,3.8,4.3,4.2,3.9,3.7,4.1],
     "Black":[15.0,16.4,13.1,12.6,11.1,11.4,11.7,10.6,12.3,12.8,12.1,11.6,12.7,11.5,9.4,11.1,10.2,11.7,7.6],
@@ -116,3 +122,109 @@ city_to_races = {
     "Weymouth":[82.5,5.9,2.9,8.7],
     "Worcester":[59.0,14.4,18.9,7.7]
 }
+
+
+city_lat_long = {
+    "Attleboro":[41.944538,-71.284088],
+    "Barnstable":[41.677690,-70.359280],
+    "Boston":[42.360081,-71.058884],
+    "Brockton":[42.083431,-71.018379],
+    "Brookline":[42.333672,-71.120880],
+    "Cambridge":[42.373615,-71.109734],
+    "Chicopee":[42.148331,-72.606956],
+    "Fall River":[41.701031,-71.155388],
+    "Framingham":[42.281559,-71.418831],
+    "Haverhill":[42.777431,-71.077332],
+    "Lawrence":[42.707035,-71.163116],
+    "Leominister":[42.525860,-71.760130],
+    "Lowell":[42.633427,-71.316170],
+    "Lynn":[42.464409,-70.948509],
+    "Malden":[42.426498,-71.073547],
+    "Medford":[42.418560,-71.106453],
+    "Methuen":[42.729851,-71.183983],
+    "New Bedford":[41.634048,-70.927658],
+    "Newton":[42.338032,-71.211578],
+    "Peabody":[42.534599,-70.928757],
+    "Pittsfield":[42.445030,-73.252800],
+    "Plymouth":[41.955750,-70.664391],
+    "Quincy":[42.252876,-71.002274],
+    "Revere":[42.409969,-71.012398],
+    "Somerville":[42.389118,-71.097153],
+    "Springfield":[42.102051,-72.585762],
+    "Taunton":[41.901680,-71.092651],
+    "Waltham":[42.376171,-71.238991],
+    "Weymouth":[42.217529,-70.936897],
+    "Worcester":[42.262119,-71.802238]
+}
+
+
+def distance(origin, destination):
+    lat1 = origin[0]
+    lon1 = origin[1]
+    lat2 = destination[0]
+    lon2 = destination[1]
+    radius = 6371 # km
+
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    d = radius * c
+
+    return d
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--city', type=str)
+    parser.add_argument('--race', type=str)
+    args = parser.parse_args()
+
+
+    if city_to_infant_mortality_rate.get(args.city) > numpy.mean(race_to_infant_mortality_rate.get(args.race)):
+
+        orig_city = city_lat_long.get(args.city)
+        city_list = []
+        distance_list = []
+        for city in city_lat_long.keys():
+            if city is not args.city:
+
+                dest_city = city_lat_long.get(city)
+                city_list.append(city)
+
+                distance_list.append(distance(orig_city,dest_city))
+        delete = city_list.index(args.city)
+        city_list.remove(args.city)
+        del distance_list[delete]
+        best_city = city_list[distance_list.index(min(distance_list))]
+        print("1")
+        list_of_hospitals = city_to_hospital_dict.get(best_city)
+        hospital_list = []
+        score_list = []
+        if len(list_of_hospitals) > 1:
+            for hospital in list_of_hospitals:
+                hospital_list.append(hospital[0])
+                score_list.append(hospital[1])
+            best_hospital = hospital_list[score_list.index(max(score_list))]
+            print(1.2)
+            print(best_hospital)
+        else:
+            print(1.3)
+            print(list_of_hospitals[0])
+    else:
+        list_of_hospitals = city_to_hospital_dict.get(args.city)
+        hospital_list = []
+        score_list = []
+        if len(list_of_hospitals) > 1:
+            for hospital in list_of_hospitals:
+                hospital_list.append(hospital[0])
+                score_list.append(hospital[1])
+            best_hospital = hospital_list[score_list.index(max(score_list))]
+            print(2)
+            print(best_hospital)
+        else:
+            print(3)
+            print(list_of_hospitals[0])
+
+if __name__ == '__main__':
+     main()
